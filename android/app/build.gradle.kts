1160dc50ac1c56bab07dad66955c97b311b29b90
mokeyCode <sanylanv@gmail.com>
2025-07-25 11:11:54 +0800

    support Android 15

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
